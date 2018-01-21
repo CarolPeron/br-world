f72ff6344b9759c00184ce644a18a3288baef2ee
caroline peron <carolineperon@gmail.com>
2018-01-21 03:45:10 -0200

    alterações styleshit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,10 @@
-//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require jquery-fileupload/basic
+//= require bootstrap-sprockets
+//= require bootstrap-datepicker
+//= require underscore
+//= require cloudinary/jquery.cloudinary
+//= require attachinary
+//= require gmaps/google
 //= require_tree .
---
+++ app/assets/javascripts/channels/datepicker.js
@@ -0,0 +1,6 @@
+$(function (){
+  $.fn.datepicker.defaults.format = "dd/mm/yyyy";
+  $.fn.datepicker.defaults.weekStart = 1;
+  $.fn.datepicker.defaults.startDate = 'd';
+  $.fn.datepicker.defaults.autoclose = true;
+});
